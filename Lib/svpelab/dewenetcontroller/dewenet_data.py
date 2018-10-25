03ad55d366de08676012b12768015b5b4abcaef4
Ron Ablinger <ron.ablinger@ait.ac.at>
2018-10-26 00:31:29 +0200

    Cleanup and small fixes for DEWEController

--- Lib/svpelab/dewenetcontroller/dewenet_data.py
+++ Lib/svpelab/dewenetcontroller/dewenet_data.py
@@ -463,6 +463,8 @@ class DeweChannelInfo:
 
         if isinstance(string_value, str):
             string_value = string_value.replace(",", ".")
+        if isinstance(string_value, unicode):
+            string_value = string_value.replace(",", ".")
         return float(string_value)  # float
 
     def __str__(self, *args, **kwargs):
