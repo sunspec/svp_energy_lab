56f666c549f4f752b63df59f892772273b14b9de
jayatsandia <jjohns2@sandia.gov>
2022-08-11 22:01:22 -0600

    Network capture script

---
+++ Scripts/net_test.py
@@ -0,0 +1,111 @@
+"""
+Copyright (c) 2017, Sandia National Labs and SunSpec Alliance
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the names of the Sandia National Labs and SunSpec Alliance nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Questions can be directed to support@sunspec.org
+"""
+
+import sys
+import os
+import traceback
+from svpelab import network
+import script
+
+def test_run():
+
+    result = script.RESULT_FAIL
+    load = None
+
+    try:
+        net = network.net_init(ts)
+        ts.log('Network capture device: %s' % net.info())
+
+        net.net_capture()
+        net.print_capture()
+
+        result = script.RESULT_COMPLETE
+
+    except script.ScriptFail as e:
+        reason = str(e)
+        if reason:
+            ts.log_error(reason)
+
+    return result
+
+
+def run(test_script):
+
+    try:
+        global ts
+        ts = test_script
+        rc = 0
+        result = script.RESULT_COMPLETE
+
+        ts.log_debug('')
+        ts.log_debug('**************  Starting %s  **************' % (ts.config_name()))
+        ts.log_debug('Script: %s %s' % (ts.name, ts.info.version))
+        ts.log_active_params()
+
+        result = test_run()
+
+        ts.result(result)
+        if result == script.RESULT_FAIL:
+            rc = 1
+
+    except Exception as e:
+        ts.log_error('Test script exception: %s' % traceback.format_exc())
+        rc = 1
+
+    sys.exit(rc)
+
+info = script.ScriptInfo(name=os.path.basename(__file__), run=run, version='1.0.0')
+
+# network parameters
+network.params(info)
+
+
+def script_info():
+    
+    return info
+
+
+if __name__ == "__main__":
+
+    # stand alone invocation
+    config_file = None
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+
+    params = None
+
+    test_script = script.Script(info=script_info(), config_file=config_file, params=params)
+    test_script.log('log it')
+
+    run(test_script)
+
+
